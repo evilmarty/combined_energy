c02d2e73f2a61e91f0f1feef347232fb1ac772ab
Marty Zalega <marty@zalega.me>
2025-04-24 01:02:43 +0000

    Refactor CombinedEnergyCoordinator to improve update methods and remove unused readings coordinator

--- custom_components/combined_energy/coordinator.py
+++ custom_components/combined_energy/coordinator.py
@@ -3,17 +3,20 @@
 from __future__ import annotations
 
 import asyncio
-from collections import deque
 from datetime import datetime
-from logging import Logger
 
+from aiohttp import ClientResponseError
 from custom_components.combined_energy.client import Client
 from custom_components.combined_energy.models import LogSession, Readings, TariffDetails
 
-from homeassistant.components.sensor import UndefinedType
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.update_coordinator import UNDEFINED, DataUpdateCoordinator
+from homeassistant.helpers.update_coordinator import (
+    UNDEFINED,
+    DataUpdateCoordinator,
+    UndefinedType,
+    UpdateFailed,
+)
 
 from .const import (
     LOG_SESSION_REFRESH_DELAY,
@@ -24,6 +27,7 @@ from .const import (
 
 CombinedEnergyLogSessionCoordinator = DataUpdateCoordinator[LogSession]
 CombinedEnergyTariffDetailsCoordinator = DataUpdateCoordinator[TariffDetails]
+CombinedEnergyReadingsCoordinator = DataUpdateCoordinator[Readings]
 
 
 class CombinedEnergyCoordinator:
@@ -36,6 +40,7 @@ class CombinedEnergyCoordinator:
         config_entry: ConfigEntry | None | UndefinedType = UNDEFINED,
     ) -> None:
         """Initialize the coordinator."""
+        self._last_range_end = None
         self.client = client
         self.log_session = CombinedEnergyLogSessionCoordinator(
             hass=hass,
@@ -43,14 +48,17 @@ class CombinedEnergyCoordinator:
             config_entry=config_entry,
             name="log_session",
             update_interval=LOG_SESSION_REFRESH_DELAY,
-            update_method=client.start_log_session,
+            update_method=self._update_log_session,
+            always_update=True,
         )
         self.readings = CombinedEnergyReadingsCoordinator(
             hass=hass,
-            client=client,
             logger=LOGGER,
             config_entry=config_entry,
+            name="readings",
             update_interval=READINGS_UPDATE_DELAY,
+            update_method=self._update_readings,
+            always_update=True,
         )
         self.tariff_details = CombinedEnergyTariffDetailsCoordinator(
             hass=hass,
@@ -58,8 +66,40 @@ class CombinedEnergyCoordinator:
             config_entry=config_entry,
             name="tariff_details",
             update_interval=TARIFF_DETAILS_UPDATE_DELAY,
-            update_method=client.tariff_details,
+            update_method=self._update_tariff_details,
+            always_update=False,
         )
+        # This ensures that the log session coordinator triggers updates
+        self.log_session.async_add_listener(
+            lambda: LOGGER.debug("Log session has been restarted")
+        )
+
+    async def _update_log_session(self) -> LogSession:
+        """Fetch log session from the API."""
+        try:
+            log_session = await self.client.start_log_session()
+        except ClientResponseError as err:
+            raise UpdateFailed from err
+        return log_session
+
+    async def _update_readings(self) -> Readings:
+        """Fetch readings from the API."""
+        try:
+            readings = await self.client.readings(
+                range_start=self._last_range_end, range_end=datetime.now()
+            )
+        except ClientResponseError as err:
+            raise UpdateFailed from err
+        self._last_range_end = readings.range_end
+        return readings
+
+    async def _update_tariff_details(self) -> TariffDetails:
+        """Fetch tariff details from the API."""
+        try:
+            tariff_details = await self.client.tariff_details()
+        except ClientResponseError as err:
+            raise UpdateFailed from err
+        return tariff_details
 
     async def async_config_entry_first_refresh(self) -> None:
         """Refresh all coordinators."""
@@ -68,45 +108,3 @@ class CombinedEnergyCoordinator:
             self.readings.async_config_entry_first_refresh(),
             self.tariff_details.async_config_entry_first_refresh(),
         )
-
-
-class CombinedEnergyReadingsCoordinator(DataUpdateCoordinator[Readings]):
-    """Coordinator for readings."""
-
-    def __init__(
-        self,
-        hass: HomeAssistant,
-        client: Client,
-        logger: Logger,
-        update_interval: int,
-        config_entry: ConfigEntry | None | UndefinedType = UNDEFINED,
-        log_session_reset_count: int = 3,
-    ) -> None:
-        """Initialize the coordinator."""
-        super().__init__(
-            hass=hass,
-            logger=logger,
-            config_entry=config_entry,
-            name="readings",
-            update_interval=update_interval,
-        )
-        self.client = client
-        self._last_range_end = None
-        self._empty = deque(
-            [False] * log_session_reset_count, maxlen=log_session_reset_count
-        )
-
-    async def _async_update_data(self) -> Readings:
-        """Fetch readings from the API."""
-        # This is a workaround for the fact that the API does not return readings but won't indicate the fact neither.
-        # Source: https://github.com/timsavage/combined-energy-api/blob/develop/src/combined_energy/helpers.py#L46
-        if all(self._empty):
-            await self.client.start_log_session()
-        readings = await self.client.readings(
-            range_start=self._last_range_end, range_end=datetime.now()
-        )
-        is_empty = readings.range_count == 0
-        self._empty.append(readings.range_count == 0)
-        if not is_empty:
-            self._last_range_end = readings.range_end
-        return readings
